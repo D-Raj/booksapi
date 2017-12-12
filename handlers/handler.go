4e3dfcb686a31e94694f00e8b195991b8c0e8197
Daniel Osineye <daniel.osineye@gmail.com>
2017-12-12 13:42:15 +0100

    Get single book working

--- handlers/handler.go
+++ handlers/handler.go
@@ -3,11 +3,13 @@ package handlers
 import (
 	"encoding/json"
 	"net/http"
+
+	"github.com/gorilla/mux"
 )
 
 // Structure of a single book
 type Book struct {
-	ID        int    `json:"id,omitempty"`
+	ID        string `json:"id,omitempty"`
 	Name      string `json:"name,omitempty"`
 	Author    string `json:"author,omitempty"`
 	Genre     string `json:"genre,omitempty"`
@@ -19,15 +21,15 @@ var books []Book
 
 // dummy function that simulates getting/initializing books from database
 func getBooks() {
-	gobook := Book{ID: 1, Name: "GoBook", Author: "Caleb Doxsey", Genre: "Programming", Publisher: "Not specified"}
-	elon := Book{ID: 2, Name: "Elon Musk", Author: "Ashley Vance", Genre: "Biography", Publisher: "Not specified"}
-	strengthInGod := Book{ID: 3, Name: "Finding strength in the word of God", Author: "Kenneth Hagin", Genre: "Spiritual", Publisher: "Not specified"}
-	java := Book{ID: 4, Name: "Java for Dummies", Author: "Barry Burd", Genre: "Programming", Publisher: "John Wiley and Sons"}
+	gobook := Book{ID: "1", Name: "GoBook", Author: "Caleb Doxsey", Genre: "Programming", Publisher: "Not specified"}
+	elon := Book{ID: "2", Name: "Elon Musk", Author: "Ashley Vance", Genre: "Biography", Publisher: "Not specified"}
+	strengthInGod := Book{ID: "3", Name: "Finding strength in the word of God", Author: "Kenneth Hagin", Genre: "Spiritual", Publisher: "Not specified"}
+	java := Book{ID: "4", Name: "Java for Dummies", Author: "Barry Burd", Genre: "Programming", Publisher: "John Wiley and Sons"}
 	// dump all the books in a single array
 	books = append(books, gobook, elon, strengthInGod, java)
 }
 
-// function that returns all books
+// AllBooks function that returns all books
 func AllBooks(w http.ResponseWriter, r *http.Request) {
 	//get all books from magical database
 	getBooks()
@@ -36,4 +38,17 @@ func AllBooks(w http.ResponseWriter, r *http.Request) {
 	// clear the array so we don't overpopulate the response on multiple calls
 	// remove the line below, make the call multiple times and see the difference
 	books = nil
+}
+
+// GetBook gets a single book by Id
+func GetBook(w http.ResponseWriter, r *http.Request) {
+	getBooks()
+	params := mux.Vars(r)
+	for _, item := range books {
+		if item.ID == params["id"] {
+			json.NewEncoder(w).Encode(item)
+			return
+		}
+	}
+	json.NewEncoder(w).Encode(&Book{})
 }
