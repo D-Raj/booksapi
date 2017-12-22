e35c4d31a1dd97ffb5f326a9dfd87ccdb2ceab2b
Daniel Osineye <daniel.osineye@gmail.com>
2017-12-22 15:38:10 +0100

    Created a dummy db|Optimize stuff|i'm a rockstar

--- app/handlers/handler.go
+++ app/handlers/handler.go
@@ -4,25 +4,17 @@ import (
 	"encoding/json"
 	"net/http"
 
+	"github.com/dharnnie/booksapi/app/db"
 	"github.com/dharnnie/booksapi/app/types"
 	"github.com/gorilla/mux"
 )
 
-// Array of books
+// Array of book
 var books []types.Book
 
 // dummy function that simulates getting/initializing books from database
 func getBooks() {
-	gobook := types.Book{ID: "1", Name: "GoBook", Author: "Caleb Doxsey", Genre: "Programming", Publisher: "Not specified"}
-	elon := types.Book{ID: "2", Name: "Elon Musk | Tesla | SpaceX and the quest for a fantastic future", Author: "Ashley Vance", Genre: "Biography", Publisher: "Not specified"}
-	strengthInGod := types.Book{ID: "3", Name: "Finding strength in the word of God", Author: "Kenneth Hagin", Genre: "Spiritual", Publisher: "Not specified"}
-	java := types.Book{ID: "4", Name: "Java for dummies", Author: "Barry Burd", Genre: "Programming", Publisher: "John Wiley and Sons"}
-	aod := types.Book{ID: "5", Name: "Acres of Diamond", Author: "Russell .H Conwell", Genre: "Inspiration", Publisher: "Not Specified"}
-	tagr := types.Book{ID: "6", Name: "Think and grow rich", Author: "Napoleon Hill", Genre: "Finance|Wealth|Inspiration", Publisher: "Not specified"}
-	wwla := types.Book{ID: "7", Name: "We will lead Africa", Author: "Gilpin-Jackson|Sarah|Judith", Genre: "Programming", Publisher: "John Wiley and Sons"}
-	// dump all the books in a single array
-	proReact := types.Book{ID: "8", Name: "Pro React", Author: "Cassio De Soussa", Genre: "Programming", Publisher: "Apress"}
-	books = append(books, gobook, elon, strengthInGod, java, aod, tagr, wwla, proReact)
+	books = db.Books()
 }
 
 // AllBooks function that returns all books
@@ -65,7 +57,8 @@ func AddBook(w http.ResponseWriter, r *http.Request) {
 	books = nil // empty the book variable
 }
 
-// DeleteBook deletes a single book. This action is not persistent. It returns a list of books excluding deleted book
+// DeleteBook deletes a single book. This action is not persistent. It returns a list of books
+// excluding deleted book
 func DeleteBook(w http.ResponseWriter, r *http.Request) {
 	books = nil
 	getBooks()
